2c998a5df0edbf0c2db951e26047c9e025f91010
Mizikova1980 <galashina@bk.ru>
2022-07-02 15:57:43 +0300

    slider, team

---
+++ js/products.js
@@ -0,0 +1,15 @@
+const slider = $('.products__list').bxSlider({
+    pager:false,
+    controls:false
+});
+
+$(".products-slider__arrow--prev").click((e) => {
+    e.preventDefault();
+
+    slider.goToPrevSlide();
+});
+
+$(".products-slider__arrow--next").click((e) => {
+    e.preventDefault();
+    slider.goToNextSlide();
+});
